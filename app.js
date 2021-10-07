614533869bd89e557b7297340b22be1e1714af74
samarjeetpatankar <samarjeetpatankar@gmail.com>
2021-10-07 11:25:23 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput=document.querySelector("#text-input");
+var outputDiv=document.querySelector("#output")
+
+var serverURL = "	https://api.funtranslations.com/translate/groot.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function clickHandler() {
+    var inputText = textInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
